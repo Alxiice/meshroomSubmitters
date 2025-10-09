5084592cd30bfa85c53e19814247e434fba185c8
Alice Sonolet <alice.sonolet@gmail.com>
2025-10-09 19:15:00 +0200

    Reworking the farm + complete task expansion system

--- meshroom/tractorSubmitter/api/tractorJobQuery.py
+++ meshroom/tractorSubmitter/api/tractorJobQuery.py
@@ -2,13 +2,9 @@
 
 import os
 import json
-from collections import namedtuple
-
-TRACTOR_JOB_URL = "http://tractor-engine/tv/#jid={jid}"
-Chunk = namedtuple("chunk", ["iteration", "start", "end"])
 
 
-tlm = None
+_tlm = None
 _tq = None
 try:
     # tractorLoginManager is a private python package
@@ -16,7 +12,7 @@ try:
     # If not available, we offer a fallback method where you need to provide
     # credentials so that Meshroom can connect to tractor
     from tractorLoginManager import TractorLoginManager
-    tlm = TractorLoginManager()
+    _tlm = TractorLoginManager()
 except ImportError:
     import tractor.api.query
     from submitterCredentialUi import getCredentials
@@ -29,11 +25,11 @@ except ImportError:
 
 def tractorQuery(func):
     def wrapper(*args, **kwargs):
-        if tlm is None:
+        if _tlm is None:
             res = func(_tq, *args, **kwargs)
             return res
         else:
-            tq = tlm.start_query()
+            tq = _tlm.start_query()
             res = func(tq, *args, **kwargs)
             tq.closeEngineClient()
         return res
@@ -113,27 +109,9 @@ def getCurrentRunningTaskInfos():
     return getTask(jid=jid, tid=tid)
 
 
-def waitForJob(jid):
-    """ 
-    Sleep while a job is not done
-    Do not use!
-    """
-    print(f"[TractorSubmitter] Block the current process to wait for completion of {jid}")
-    import time
-    while True:
-        time.sleep(5)
-        job = getJob(jid)
-        if job.get("numactive", 0) != 0:
-            continue
-        if job.get("numerror", 0) != 0:
-            continue
-        if job.get("numdone", 0) == job.get("maxtid"):
-            break
-
 # 
 # Job actions
 # 
-
 
 @tractorQuery
 def pauseJob(tq, jid):
